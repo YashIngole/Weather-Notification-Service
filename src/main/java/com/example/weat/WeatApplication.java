b4e4925f1758d0db4dcde7d55e0edebe439acdc2
YashIngole <yashingole1000@gmail.com>
2023-06-13 20:46:16 +0530

    configured email service

--- src/main/java/com/example/weat/WeatApplication.java
+++ src/main/java/com/example/weat/WeatApplication.java
@@ -2,9 +2,11 @@ package com.example.weat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@ComponentScan({"com.example.weat", "com.example.MailConfig"})
 public class WeatApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/weat/WeatherController.java
+++ src/main/java/com/example/weat/WeatherController.java
@@ -1,5 +1,6 @@
 package com.example.weat;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +14,9 @@ public class WeatherController {
 
     private static final List<String> CITIES = Arrays.asList("Nagpur", "Pune", "Mumbai", "Delhi", "Chennai", "Kolkata", "Bangalore");
 
+    @Autowired
+    private EmailService emailService;
+
     @GetMapping("/weather")
     public List<String> getWeatherForCities() {
         String apiKey = "75489b6e94cec5e9f80c09786521e786";
@@ -24,6 +28,12 @@ public class WeatherController {
             String result = restTemplate.getForObject(url, String.class);
             weatherInfoList.add(result);
         }
+
+        // send the weather information as an email
+        String to = "20010970@ycce.in"; // replace with the recipient email address
+        String subject = "Weather Information";
+        String body = String.join("\n\n", weatherInfoList);
+        emailService.sendEmail(to, subject, body);
 
         return weatherInfoList;
     }
